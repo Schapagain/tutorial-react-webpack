203351e488c238f1601bb223d24688fe6b111a4a
Sandesh <chapagainsandesh88@gmail.com>
2021-04-24 15:10:25 -0400

    add css- and style- loaders

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
