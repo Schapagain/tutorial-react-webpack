07b66a73e54e483afb193f9b6902672464e16dcc
Sandesh <chapagainsandesh88@gmail.com>
2021-04-24 14:32:25 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    publicPath: "/",
+    contentBase: "./dist",
+    hot: true,
+    open: true,
+    watchOptions: {
+      ignored: /node_modules/,
+      poll: 1000,
+    },
+    port: 9000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
